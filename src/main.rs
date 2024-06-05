01e2ee4f5d9a222b8a063d9d8b7f4761ac0ec81a
Daniel Southward-ellis <southwardellis.daniel@gmail.com>
2024-06-05 19:10:53 +1200

    Use Embassy

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 #![no_std]
 #![no_main]
+#![feature(impl_trait_in_assoc_type)]
 
+// Setup logging
 use defmt_rtt as _;
+use embassy_time::Timer;
 use panic_probe as _;
 
-use nrf52832_hal as hal;
+use embassy_executor::Spawner;
 
-#[cortex_m_rt::entry]
-fn main() -> ! {
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let _ = embassy_nrf::init(Default::default());
+    defmt::info!("Starting");
+
+    let _ = spawner.spawn(every_second());
+}
+
+#[embassy_executor::task]
+async fn every_second() -> ! {
     let mut i: u64 = 0;
     loop {
-        defmt::info!("Hello, world! Count={}", i);
+        defmt::info!("Hello, world! Seconds since boot={}", i);
         i += 1;
-
-        if i > 10000 {
-            panic!("Oops");
-        }
+        Timer::after_secs(1).await;
     }
 }
